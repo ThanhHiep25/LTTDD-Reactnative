294c7d8ca7799d4518de0aebcfb0e19482fd9aff
Nguyenthanhhiep <87129597+ThanhHiep25@users.noreply.github.com>
2023-10-19 13:47:09 +0700

    create datahome

---
+++ Recipe-food-app/datahome.js
@@ -0,0 +1,14 @@
+const datahome = [
+    {
+        id:1,
+        name:'Lẩu cá chép giòn :',
+        img: require('./assets/IMG/home/lau-ca-chep-gion-chua-ngot-hai-hoa.png')
+    },
+    {
+        id:1,
+        name:'Lẩu cá chép giòn :',
+        img: require('./assets/IMG/home/chao-suon.png')
+    }
+]
+
+export default datahome;
